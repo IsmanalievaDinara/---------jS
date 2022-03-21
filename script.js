b955524ee57893584721528147d5fe48ed6c75c8
Dinara Ismanalieva <93486540+IsmanalievaDinara@users.noreply.github.com>
2022-03-21 17:45:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const firstNum = document.querySelector('#firstNum')
-const secondNum = document.querySelector('#secondNum')
-const button = document.querySelector('#btn')
-resultDiv = document.querySelector('#result')
 
-function multiplyFunc(event) {
-    let sum = +(firstNum.value) + + (secondNum.value)
-    resultDiv.innerHTML = sum
-}
  
 
