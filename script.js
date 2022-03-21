ff4033a0a3072ea51c7b60e3febeef041572a8a3
Dinara <ismanalievadina@gmail.com>
2022-03-21 18:29:14 +0300

    func added

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-const firstNum = document.querySelector('#firstNum')
-const secondNum = document.querySelector('#secondNum')
-const button = document.querySelector('#btn')
-resultDiv = document.querySelector('#result')
+let button = document.querySelector("#changeColorBtn")
+let span = document.querySelector("span")
+let main = document.querySelector("main")
+let hex = document.querySelector("#hex")
+let simple = document.querySelector("#simple")
 
-function multiplyFunc(event) {
-    let sum = +(firstNum.value) + + (secondNum.value)
-    resultDiv.innerHTML = sum
-}
+let simpleIsOn = true;
+let hexIsOn = false;
+
+let simpleColor = ["red", "blue", "magenta", "black", "orange","pink","green"]
+
+hex.addEventListener("click",function () {
+    hex.classList.add("active")
+    simple.classList.remove("active")
+    simpleIsOn = false;
+    hexIsOn = true;
+})
+
+simple.addEventListener("click", function(){
+    hex.classList.remove("active")
+    simple.classList.add("active")
+    simpleIsOn = true;
+    hexIsOn = false;
+})
+
+button.addEventListener("click", function () {
+    if(simpleIsOn) {
+        let randomIndex = Math.floor(Math.random() * simpleColor.length)
+        let randomColor = simpleColor[randomIndex]
+        console.log(randomColor)
+        span.innerHTML = randomColor;
+        main.style.backgroundColor = randomColor
+    }else {
+        let randomHex = `#${Math.floor(Math.random() * 16777215).toString(16)}`    
+        span.innerHTML = randomHex;
+        main.style.backgroundColor = randomHex;
+    
+    }
+
+})
+
+
  
 
